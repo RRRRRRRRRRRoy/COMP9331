02dd3dc3d87d6d7e4ccb497ff8185326d1c8dd22
RRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2020-10-05 19:21:07 +1100

    lab2-test2

--- Lab2/PingClient.py
+++ Lab2/PingClient.py
@@ -5,8 +5,9 @@ import time
 # Change the wait delay if needed
 # 600ms 
 wait_delay = 0.6
+min_result = max_result = avg_result = 0
 
-if len(sys.argv) ==1:
+if len(sys.argv) <= 1:
     print("Need arguments for Address and Port No!")
     sys.exit()
 
@@ -17,7 +18,9 @@ if len(sys.argv) ==1:
 # we can also get the port number from the third argument of command-line, which is sys.argv[2]
 #####################################################################################################
 server_name = sys.argv[1]
-port_number = int(sys.argv[2])
+string_port_num = sys.argv[2]
+port_number = int(string_port_num)
+address_info = (server_name,port_number)
 
 
 #####################################################################################################
@@ -43,23 +46,34 @@ for index in range(15):
     send_time = time.time()
     blank_str = " " 
     str_message = "PING" + blank_str + str(index + seq_number) + blank_str + str(send_time) + "\r\n"
-    client_socket.sendto(str_message.encode("utf-8"),(server_name,port_number))
-    time_2_receive = time.time() + wait_delay
+    message_2_UTF8 = str_message.encode("utf-8")
+    client_socket.sendto(message_2_UTF8,address_info)
+
     # wait_dely to receive
-    while time.time() <= time_2_receive:
-        try:
-            modified_message,server_address = client_socket.recvfrom(2048)
-        except socket.error as error:
-            # continue to receive from socket until get nothing
-            continue
-        if modified_message is True and server_address is True:
-            m_rece = modified_message.decode('utf-8')[5:]
-            if (index + seq_number) == int(m_rece[:m_rece.index(' ')]):
-                result_list.append((time.time()-send_time)*1000)
-                print(f"ping to {server_name}, seq = {seq_number + index}, rtt={round(result_list[count])}ms")
-                count += 1
-                break
+    while time.time() > send_time + wait_delay:
+            print(f"ping to {server_name}, seq = {seq_number + index}, time out")
     else:
-        print(f"ping to {server_name}, seq = {seq_number + index}, time out")
-print(f"min: {round(min(result_list))}ms, max: {round(max(result_list))}ms, avg: {round(sum(result_list)/len(result_list))}ms")
+        try:
+            modified_message = client_socket.recvfrom(2048)[0]
+            server_address = client_socket.recvfrom(2048)[1]
+        except socket.error as error:
+                # continue to receive from socket until get nothing
+            continue
+        if modified_message:
+            if server_address:
+                decode_message = modified_message.decode('utf-8')
+                m_rece = decode_message[5:]
+                get_blank_mrece = m_rece.index(' ')
+                if (index + seq_number) == int(m_rece[0:get_blank_mrece]):
+                    rtt_time = time.time()*1000 - send_time*1000
+                    result_list.append(rtt_time)
+                    print(f"ping to {server_name}, seq = {seq_number + index}, rtt={round(result_list[count])}ms")
+                    count += 1
+                    break
+
+min_result = min([item for item in result_list])
+max_result = max([item for item in result_list])
+avg_result = sum([item for item in result_list]) / len(result_list)
+
+print(f"min: {round(min_result)}ms, max: {round(max_result)}ms, avg: {round(avg_result)}ms")
 client_socket.close()
--- Lab2/test.py
+++ Lab2/test.py
@@ -1,7 +1,5 @@
-list1 = [1,2,3,4,5,6,7]
-print(min(list1))
-list1.append(7)
-print(type(list1[7]))
+#
 
-address = (1,2)
-print(type(address))
+test = [1,2,3,4]
+
+print(sum([x for x in test]))
