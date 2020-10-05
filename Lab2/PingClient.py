4fe3f9a79a51033328bd94267c79116cbd724288
RRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2020-10-05 14:41:21 +1100

    lab2_finish

--- Lab2/PingClient.py
+++ Lab2/PingClient.py
@@ -35,8 +35,8 @@ client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client_socket.settimeout(0)
 
 result_list = list()
-count = -1
-for index in range(0,10):
+seq_number = 3331
+for index in range(0,15):
     # record the time sending ---> time stamp
     send_time = time.time()
     blank_str = " " 
@@ -44,6 +44,22 @@ for index in range(0,10):
     message_2_UTF8 = str_message.encode("utf-8")
     client_socket.sendto(message_2_UTF8,Address)
     time_2_receive = time.time() + wait_delay
+    # wait_dely to receive
     while time.time() <= time_2_receive:
         try:
             modified_message,server_address = client_socket.recvfrom(2048)
+        except socket.error as error:
+            # continue to receive from socket until get nothing
+            continue
+        if modified_message is True and server_address is True:
+            modified_receive = modified_message.decode('utf-8')[5:]
+            receive_index = modified_receive.index(' ')
+            if index == int(modified_receive[:receive_index]):
+                result_list.append((time.time()-send_time)*1000)
+                seq_number += 1
+                print(f"ping to {server_name}, seq = {index}, rtt={round(result_list[seq_number])}ms")
+                break
+        else:
+            print(f"ping to {server_name}, seq = {index}, time out")
+print(f"min: {round(min(result_list))}ms, max: {round(max(result_list))}ms, avg: {round(sum(result_list)/len(result_list))}ms")
+client_socket.close()
