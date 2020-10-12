b5405862d6772f6f25a258a8925af2a5a93cb7f8
RRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2020-10-12 16:21:47 +1100

    Lab4

--- Lab3/WebServer.py
+++ Lab3/WebServer.py
@@ -18,13 +18,19 @@ server_socket.listen(1)
 print("Server is ready!")
 
 header_type = {200 : "HTTP/1.1 200 OK\r\nAcept-Ranges: bytes\r\nKeep-Alive: timeout=10, max=100\r\nConnection: " \
-                                                            "Keep-Alive\r\nContent-Type: " \
-                                                            "text/html\r\n\r\n",\
-                                                            404 : "HTTP/1.1 404 Not Found\r\nAcept-Ranges: bytes\r\nKeep-Alive: timeout=10, max=100\r\nConnection: " \
-                                                            "Keep-Alive\r\nContent-Type: " \
-                                                            "text/html\r\n\r\n"}
+                    "Keep-Alive\r\nContent-Type: " \
+                    "text/html\r\n\r\n",\
+                    404 : "HTTP/1.1 404 Not Found\r\nAcept-Ranges: bytes\r\nKeep-Alive: timeout=10, max=100\r\nConnection: " \
+                    "Keep-Alive\r\nContent-Type: " \
+                    "text/html\r\n\r\n",\
+                    "Not_Found_page":'<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1> <p>The requested ' \
+			        'URL was not found on this server.</p></body></html>'}
 
 
-Not_Found = '<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1> <p>The requested ' \
-			'URL was not found on this server.</p></body></html>'
-
+while True:
+    connection_socket,address = [item for item in server_socket.accept()]
+    sentence = connection_socket.recv(1024).decode()
+    if sentence:
+        start_index = sentence.index("/")
+        end_index = sentence.index(" HTTP")
+        URL = sentence[start_index + 1 : end_index]
