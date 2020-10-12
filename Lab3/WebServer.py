8e4e583fd2f79c762deb673b5f64cb8248030271
RRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2020-10-12 11:59:19 +1100

    Lab4

--- Lab3/WebServer.py
+++ Lab3/WebServer.py
@@ -7,4 +7,19 @@ if sys.version_info[0] < 3:
 
 server_port = int(sys.argv[1])
 
-server_socket = socket(AF_INET,SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server_socket.listen(1)
+
+print("Server is ready!")
+
+while True:
+    connection_socket = server_socket.accept()[0]
+    address = server_socket.accept()[1]
+    sentence = connection_socket.recv(2048).decode()
+    if sentence:
+        URL = sentence[sentence.index("/") + 1 : sentence.index(" HTTP")]
+        try:
+            get_file = open(URL,'rb')
+        except FileNotFoundError:
+            
