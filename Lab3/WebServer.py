5d624b50c203396a756648229cdb293f20f5fdb1
RRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2020-10-12 16:13:32 +1100

    Lab4

--- Lab3/WebServer.py
+++ Lab3/WebServer.py
@@ -1,25 +1,30 @@
 import base64
 import socket
-import time, sys
+import time
+import sys
 
 if sys.version_info[0] < 3:
     raise Exception("Please use Python3 file as the input!")
 
 server_port = int(sys.argv[1])
+IP_info = ('localhost',server_port)
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server_socket.bind(IP_info)
 
 server_socket.listen(1)
 
 print("Server is ready!")
 
-while True:
-    connection_socket = server_socket.accept()[0]
-    address = server_socket.accept()[1]
-    sentence = connection_socket.recv(2048).decode()
-    if sentence:
-        URL = sentence[sentence.index("/") + 1 : sentence.index(" HTTP")]
-        try:
-            get_file = open(URL,'rb')
-        except FileNotFoundError:
-            
+header_type = {200 : "HTTP/1.1 200 OK\r\nAcept-Ranges: bytes\r\nKeep-Alive: timeout=10, max=100\r\nConnection: " \
+                                                            "Keep-Alive\r\nContent-Type: " \
+                                                            "text/html\r\n\r\n",\
+                                                            404 : "HTTP/1.1 404 Not Found\r\nAcept-Ranges: bytes\r\nKeep-Alive: timeout=10, max=100\r\nConnection: " \
+                                                            "Keep-Alive\r\nContent-Type: " \
+                                                            "text/html\r\n\r\n"}
+
+
+Not_Found = '<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1> <p>The requested ' \
+			'URL was not found on this server.</p></body></html>'
+
