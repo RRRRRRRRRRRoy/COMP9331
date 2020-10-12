adc3050df3100617747a27f16949c115fe48ab14
RRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2020-10-12 16:56:53 +1100

    Lab4

--- Lab3/WebServer.py
+++ Lab3/WebServer.py
@@ -6,17 +6,22 @@ import sys
 if sys.version_info[0] < 3:
     raise Exception("Please use Python3 file as the input!")
 
+# get the IP address and server port
+# wrap them into a tuple
 server_port = int(sys.argv[1])
 IP_info = ('localhost',server_port)
 
+################################################################################################
+# The same way like writing the PingClient
+# Different : PingClient is UDP , WebServer is TCP which needs to bind and set up the connection
+################################################################################################
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
 server_socket.bind(IP_info)
-
 server_socket.listen(1)
-
+# show the result in the terminal -----> Server is ready
 print("Server is ready!")
 
+# Using the dictionary is easier than using String
 header_type = {200 : "HTTP/1.1 200 OK\r\nAcept-Ranges: bytes\r\nKeep-Alive: timeout=10, max=100\r\nConnection: " \
                     "Keep-Alive\r\nContent-Type: " \
                     "text/html\r\n\r\n",\
@@ -26,20 +31,65 @@ header_type = {200 : "HTTP/1.1 200 OK\r\nAcept-Ranges: bytes\r\nKeep-Alive: time
                     "Not_Found_page":'<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1> <p>The requested ' \
 			        'URL was not found on this server.</p></body></html>'}
 
+# Using this function to chenge string to utf-8 bytes
+def change_bytes(key,header_type):
+    bytes_value = bytes(header_type[key],'utf-8')
+    return bytes_value
 
+################################################################################################
+# From the http header
+# The file name is behind '/' before " HTTP"
+# getting the index of these characters and using the slice method to cut it
+# Source: https://stackoverflow.com/questions/8035900/how-to-get-filename-from-content-disposition-in-headers
+################################################################################################
 while True:
     connection_socket,address = [item for item in server_socket.accept()]
-    sentence = connection_socket.recv(1024).decode()
+    sentence = connection_socket.recv(2048).decode()
     if sentence:
         start_index = sentence.index("/")
         end_index = sentence.index(" HTTP")
         file_name = sentence[start_index + 1 : end_index]       
-        
+################################################################################################
+# Check whether the file is in the directory
+# Using the try-catch function
+# exist -----> OK -----> send name to server and show the resource
+# Not exist -----> NOT Found -----> show the NOT FOUND page(in the previous dictionary)
+# You can also find this function in Stack Overflow
+# Source: https://stackoverflow.com/questions/22366282/python-filenotfound
+################################################################################################
         try:
             get_file = open(file_name,'rb')
         except FileNotFoundError:
-            connection_socket.send(bytes(header_type[404],'utf-8'))
-            connection_socket.send(bytes(header_type["Not_Found_page"],'utf-8'))
+            connection_socket.send(change_bytes(404,header_type))
+            connection_socket.send(change_bytes("Not_Found_page",header_type))
+            print("Getting the request Fail")
+            # not found then close socket
             connection_socket.close()
+            continue
 
+################################################################################################
+# Question Source: https://webcms3.cse.unsw.edu.au/COMP3331/20T3/resources/51954
+# Only one kind of test HTML file which is index.html
+# just check whether the name of file is same or not
+################################################################################################
+        connection_socket.send(change_bytes(200,header_type))
+        if file_name == "index.html":
+            file_content = get_file.read()
+            connection_socket.send(file_content)
+            print("Getting the request successfully")
+################################################################################################
+# Second situation ------> showing picture
+# Use base64 to encode picture and show on the website
+# Step 1 : Read file with buffer reader
+# Step 2 : using base64.b64encode to encode the picture
+# Step 3 : Send the encode pic to the browser
+# Source: https://stackoverflow.com/questions/3715493/encoding-an-image-file-with-base64
+################################################################################################
+        else:
+            picture_content = get_file.read()
+            pic_encode = base64.b64encode(picture_content)
+            # Picture with HTML tag ----> Show the picture
+            connection_socket.send(b'<img src="data:image/jpg;base64,' + pic_encode + b'">')
+            print("Getting the request successfully")
+    # After getting the request, showing the content and clost socket
     connection_socket.close()
