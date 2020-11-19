d7476d5bd77851e1e22c0f091137fad4033e0005
RRRRRRRRRRRoy <RRRRRRRRRRRoy720@gmail.com>
2020-11-19 11:15:26 +0800

    Create the client Read the User in Server

--- Assignment/client.py
+++ Assignment/client.py
@@ -9,6 +9,7 @@ import datetime
 #############################################################################################
 ################    Check the python version in the terminal    #############################
 #############################################################################################
+# This part is similar with starting server
 if sys.version_info[0] >= 3:
     # The python version is 3 or not
     print("Welcome to use the Client!")
@@ -17,3 +18,14 @@ elif sys.version_info[0] < 3:
     # This time the Server should show the exception(The wrong message)
     wrong_msg = "The current Client file is written by Python3. Plz change the command and Try again!"
     raise Exception(wrong_msg)
+
+ServerName = [sys.argv[i] for i in range(1,2)]
+Serverport = int([sys.argv[i] for i in range(2,3)])
+Address_infor = (ServerName, Serverport)
+# Can also change to 127.0.0.1
+Client_server = 'localhost'
+
+# Create the client
+# Source: https://stackoverflow.com/questions/48406991/basic-python-tcp-socket-server-client
+Clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Clientsocket.connect(Address_infor)
--- Assignment/server.py
+++ Assignment/server.py
@@ -16,3 +16,33 @@ elif sys.version_info[0] < 3:
     # This time the Server should show the exception(The wrong message)
     wrong_msg = "The current Server file is written by Python3. Plz change the command and Try again!"
     raise Exception(wrong_msg)
+
+#############################################################################################
+################   Get the User Data from Credentials.txt file  #############################
+#############################################################################################
+# When we Login in the server we should get the Username and PWD from the file
+# This process can be change by connecting to the Database and get data from it
+client_dictionary = dict()
+
+# This set may used to check whether a second person want to login in the current username
+current_client = set()
+
+# record the current threads and threadtile
+threads_content = dict()
+
+# This may used to save the current thread for LST command
+LST_server = list()
+
+# Usingt with open can omit the step of Writing Close
+# Here is the reason of using with
+# Source: https://stackoverflow.com/questions/21275836/if-youre-opening-a-file-using-the-with-statement-do-you-still-need-to-close
+with open("credentials.txt") as file:
+    for index in range(len(file.readlines())):
+        if file.readlines[index]:
+            line = file.readlines[index].strip('\n')
+            # The format is : Username  Password
+            # Slice the String and get username pwd
+            U_name = line[:line.index(" ")]
+            U_pass = line[line.index(" ") + 1:]
+            # create them into a dictionary
+            client_dict[U_name] = U_pass
